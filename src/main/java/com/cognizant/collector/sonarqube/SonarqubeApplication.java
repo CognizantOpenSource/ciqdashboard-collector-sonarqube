877ce18dc62d7f4949ba5e112861f78c1497baf9
138497 <subramani.murthy@cognizant.com>
2020-10-27 04:24:46 -0700

    updated

--- src/main/java/com/cognizant/collector/sonarqube/SonarqubeApplication.java
+++ src/main/java/com/cognizant/collector/sonarqube/SonarqubeApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @EnableFeignClients
 @SpringBootApplication
 @EnableScheduling
+@EnableAutoConfiguration
+@EnableEncryptableProperties
 public class SonarqubeApplication {
 
 	public static void main(String[] args) {
