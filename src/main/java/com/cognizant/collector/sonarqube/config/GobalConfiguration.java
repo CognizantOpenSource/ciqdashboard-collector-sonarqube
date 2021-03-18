99fdc803b86766010f51a3e92715ca41eb9070e5
Shankar Mahadevan <Shankar.Mahadevan2@cognizant.com>
2021-03-18 17:27:53 +0530

    License and author update
    License and author update in all java files

--- src/main/java/com/cognizant/collector/sonarqube/config/GobalConfiguration.java
+++ src/main/java/com/cognizant/collector/sonarqube/config/GobalConfiguration.java
@@ -1,3 +1,20 @@
+
+/*
+ * © [2021] Cognizant. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http:www.apache.orglicensesLICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.cognizant.collector.sonarqube.config;
 
 
@@ -14,6 +31,11 @@ import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/*
+ * GobalConfiguration
+ *
+ * @author Cognizant
+ */
 
 @Configuration
 @Slf4j
